c5c59cfce9fa490d4769a839e5f395249e232266
jwheller0013 <jwheller0013@yahoo.com>
2025-03-21 14:21:41 -0400

    Completed minus TDD

--- gradebook.py
+++ gradebook.py
@@ -58,8 +58,46 @@ class college(Student):
 
 class Classroom:
 
-Jim = Instructor("Jim", "Heller", "1/13/1986", AliveStatus.Alive)
-print (Jim.instructor_id)
+    def __init__(self):
+        self.students = {}
+        self.instructors= {}
 
-Bob = Student("Bob", "Bobby", "1/12/23", AliveStatus.Alive)
-print(Bob.student_id)
+    def add_instructor (self, x):
+        self.instructors[x.instructor_id] = (x.first_name, x.last_name, x.dob, x.alive)
+
+    def add_student (self, y):
+        self.students[y.student_id] = (y.first_name, y.last_name, y.dob, y.alive)
+
+    def remove_instructor (self, z):
+        del self.instructors[z]
+
+    def remove_student (self, x):
+        del self.students[x]
+
+    def print_instructor (self):
+        print(self.instructors)
+
+    def print_students (self):
+        print(self.students)
+
+
+#testing parts
+
+# jim = Instructor("Jim", "Heller", "1/13/1986", AliveStatus.Alive)
+# angel = Instructor('Angel', 'Angel', "1/1/2004", AliveStatus.Alive)
+# isiah = Student('Isiah', 'Something', '1/1/2020', AliveStatus.Alive)
+# print (jim.instructor_id)
+# test_class = Classroom()
+#
+# test_class.add_instructor(jim)
+# test_class.add_instructor(angel)
+# test_class.add_student(isiah)
+#
+# test_class.print_students()
+# test_class.print_instructor()
+#
+#
+# test_class.remove_instructor(jim.instructor_id)
+# test_class.remove_student(isiah.student_id)
+# test_class.print_instructor()
+# test_class.print_students()
